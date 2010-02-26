2d0a06df5e52c288de37cbf4324fc12ee8e2e428
jcbrand <jcbrand@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-02-26 21:58:46 +0000

    Register new viewlets for the bottom.feeder panel

--- bottom/feeder/browser/viewlets.py
+++ bottom/feeder/browser/viewlets.py
@@ -1,15 +1,18 @@
 from zope.interface import implements
 from zope.viewlet.interfaces import IViewlet
 
+
 from plone.app.viewletmanager.manager import OrderedViewletManager
 
-from Products.CMFPlone.utils import safe_unicode
 from Products.CMFCore.utils import getToolByName
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 class BottomFeederViewlet(BrowserView):
-    """ """
+    """ This viewlet is registered for and rendered inside the IPortalFooter
+        viewletmanager. It's sole purpose is to render the
+        BottomFeederViewletManager.
+    """
     template = ViewPageTemplateFile('templates/bottomfeeder_viewlet.pt')
     
     def __init__(self, context, request, view, manager):
@@ -25,9 +28,15 @@ class BottomFeederViewlet(BrowserView):
 
 
 class BottomFeederViewletManager(OrderedViewletManager):
-    """ """
+    """ Any links or widgets that should appear in the bottom.feeder panel must
+        be registered as viewlets for this viewletmanager.
+    """
     template = ViewPageTemplateFile('templates/bottomfeeder.pt')
 
+
+class ViewletMixin:
+    """ Utility methods for viewlets
+    """
     def member(self):
         portal_membership = getToolByName(self.context, 'portal_membership')
         return portal_membership.getAuthenticatedMember()
@@ -36,15 +45,14 @@ class BottomFeederViewletManager(OrderedViewletManager):
         portal_url = getToolByName(self.context, 'portal_url')
         return portal_url()
 
-    def render(self):
-        return self.template()
 
 
-class RecentActivityViewlet(BrowserView):
+class HomeViewlet(BrowserView, ViewletMixin):
+    """ """
     implements(IViewlet)
 
     def __init__(self, context, request, view, manager):
-        super(RecentActivityViewlet, self).__init__(context, request)
+        super(HomeViewlet, self).__init__(context, request)
         self.__parent__ = view
         self.context = context
         self.request = request
@@ -54,7 +62,68 @@ class RecentActivityViewlet(BrowserView):
     def update(self):
         pass
 
-    def render(self):
-        snippet = safe_unicode("HELLO")
-        return snippet
+
+class UserViewlet(BrowserView, ViewletMixin):
+    """ """
+    implements(IViewlet)
+
+    def __init__(self, context, request, view, manager):
+        super(UserViewlet, self).__init__(context, request)
+        self.__parent__ = view
+        self.context = context
+        self.request = request
+        self.view = view
+        self.manager = manager
+
+    def update(self):
+        pass
+
+
+class PersonalizeViewlet(BrowserView, ViewletMixin):
+    """ """
+    implements(IViewlet)
+
+    def __init__(self, context, request, view, manager):
+        super(PersonalizeViewlet, self).__init__(context, request)
+        self.__parent__ = view
+        self.context = context
+        self.request = request
+        self.view = view
+        self.manager = manager
+
+    def update(self):
+        pass
+
+
+class MembersViewlet(BrowserView, ViewletMixin):
+    """ """
+    implements(IViewlet)
+
+    def __init__(self, context, request, view, manager):
+        super(MembersViewlet, self).__init__(context, request)
+        self.__parent__ = view
+        self.context = context
+        self.request = request
+        self.view = view
+        self.manager = manager
+
+    def update(self):
+        pass
+
+
+class NewsViewlet(BrowserView, ViewletMixin):
+    """ """
+    implements(IViewlet)
+
+    def __init__(self, context, request, view, manager):
+        super(NewsViewlet, self).__init__(context, request)
+        self.__parent__ = view
+        self.context = context
+        self.request = request
+        self.view = view
+        self.manager = manager
+
+    def update(self):
+        pass
+
 
